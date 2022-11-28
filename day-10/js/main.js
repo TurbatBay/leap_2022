d30d0cfd60b3704f93f6f5e8c3b76120f7d0f11d
Morning <morning@mstars-lab2-14s-iMac.local>
2022-11-28 12:26:15 +0800

    Day-10 angid Rest of the code

--- day-10/js/main.js
+++ day-10/js/main.js
@@ -2,7 +2,7 @@ console.log('Hello javascript');
 console.log('Im Turu');
 console.log('Im Developer');
 // alert("Hello World im using Alert")
-var hello ;
+var hello;
 
 console.log(hello);
 
@@ -18,7 +18,7 @@ console.log(myNumber);
 myNumber = 5;
 console.log(myNumber);
 
-let x = 8, y=6;
+let x = 8, y = 6;
 
 console.log(x + y);
 
@@ -28,8 +28,21 @@ console.log(x * x + y * y);
 
 const PI = 3.14;
 
-console.log (PI);
-
-PI = 6.24;
 console.log(PI);
+
+
+// PI = 6.24;
+// console.log(PI);
+
+let a = 6;
+let b = 8;
+
+
+let message = "Hi, i added ";
+let secondMessage = "It is ";
+let sum = (a + b);
+let plus = (" + ");
+alert(message + (a) + plus + (b));
+alert(secondMessage + sum);
+
 
