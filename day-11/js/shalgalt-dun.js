a8eae5f97c31fe22853ab21540880ec83f7f65e2
TurbatBay <learnwithturu@gmail.com>
2022-11-29 22:15:19 +0800

    Shalgaltiin dun duusgalaa

---
+++ day-11/js/shalgalt-dun.js
@@ -0,0 +1,43 @@
+let bat = 67 , onts = 59;
+if (onts < 60) {
+    if (onts < 60) {
+        console.log("tentseegui")
+    } else {
+        console.log("tentssen")
+    }
+    console.log("onts " + "F " + onts)
+}
+ else if (onts < 70) {
+    console.log("onts  " + "D " + onts)
+}
+else if (onts < 80) {
+    console.log("onts " + "C " + onts)
+}
+else if (onts < 90) {
+    console.log("onts " + "B " + onts)
+}
+else if (onts <= 100) {
+    console.log("onts " + "A " + onts)
+}
+
+
+if (onts < 60) {
+    if (bat < 60) {
+        console.log("tentseegui")
+    } else {
+        console.log("tentssen")
+    }
+    console.log("bat " + "F " + bat)
+}
+ else if (bat < 70) {
+    console.log("bat  " + "D " + bat)
+}
+else if (bat < 80) {
+    console.log("bat " + "C " + bat)
+}
+else if (bat < 90) {
+    console.log("bat " + "B " + bat)
+}
+else if (bat <= 100) {
+    console.log("bat " + "A " + bat)
+}
