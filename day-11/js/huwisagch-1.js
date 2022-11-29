ea7923540d9eeee6fa09d5035e5ceb1b29722291
TurbatBay <learnwithturu@gmail.com>
2022-11-29 21:04:01 +0800

    huwisagch-1,2 duusgalaa

---
+++ day-11/js/huwisagch-1.js
@@ -0,0 +1,19 @@
+// huwisagch-1
+let a = 85 , b = 75 , c = 96 , d = 69 , sum=0;
+
+if (a >= 80) sum=sum+a
+if (b >= 80) sum=sum+b
+if (c >= 80) sum=sum+c
+if (d >= 80) sum=sum+d
+console.log("huwisagch-1 hariu: " + sum)
+
+// huwisagch-2 bodlogo
+
+let e = 3 , f = 7, g = 2 , h = 4 , urjwer = 1 ;
+
+if (e < 5) urjwer = urjwer * e 
+if (f < 5) urjwer = urjwer * f 
+if (g < 5) urjwer = urjwer * g 
+if (h < 5) urjwer = urjwer * h
+
+console.log("huwisagch-2 hariu: " + urjwer)
--- day-11/js/temtseen.js
+++ day-11/js/temtseen.js
@@ -2,8 +2,8 @@
 
 let a;
 let b;
-a = (1000)/3;
-b = (12000)/3;
+a = (96 + 108 + 89)/3;
+b = (88 + 91 + 110)/3;
 console.log("a bagiin dundaj onoo " + a)
 console.log("b bagiin dundaj onoo " + b)
 if(a>=100 && b>=100){
