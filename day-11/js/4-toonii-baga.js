c0e1f08f10792c2350f96768de868ac10a6d9282
TurbatBay <learnwithturu@gmail.com>
2022-11-29 21:44:27 +0800

    4 toonii baga duusgalaa

---
+++ day-11/js/4-toonii-baga.js
@@ -0,0 +1,15 @@
+// 4 toonii baga
+let a = 120 , b = 33 , c = 10 , d = 12 ;
+
+if (a < b && a < c && a < d) {
+    console.log(a);
+}
+else if (b < a && b < c && b < d) {
+    console.log(b);
+}
+else if (c < a && c < b && c < d) {
+    console.log(c);
+} else {
+        console.log(d);
+}
+
