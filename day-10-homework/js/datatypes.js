74044cf9bec01930bfbe10b0ecba73ffa3f7db18
TurbatBay <learnwithturu@gmail.com>
2022-11-29 08:15:38 +0800

    homework zarimiig n hiile

--- day-10-homework/js/datatypes.js
+++ day-10-homework/js/datatypes.js
@@ -2,3 +2,6 @@ console.log(typeof true);
 console.log(typeof 13);
 console.log(typeof "hello world");
 console.log(typeof null);
+
+
+
--- day-10-homework/js/main.js
+++ day-10-homework/js/main.js
@@ -5,26 +5,82 @@
 // let country;
 // let age;
 //9r daalgawar//
-let firstName, lastName, maritalStatus, country, age;
+// let firstName, lastName, maritalStatus, country, age;
 
-let myAge = 14;
-let yourAge = 16;
+// let myAge = 14;
+// let yourAge = 16;
 
-let a = 5, b = 6, c = 2;
-perimeter = (a + b + c)
-console.log(perimeter);
+// let a = 5, b = 6, c = 2;
+// perimeter = (a + b + c)
+// console.log(perimeter);
 
-let y2 = 10, y1 = 2, x2 = 4, x1 = 2;
-m = ((y2 - y1) / (x2 - x1))
-console.log(m);
-let bit;
-gb15 = ((15 * 1024 * 1024 * 8000) + (" bit") );
-console.log(gb15);
+// let y2 = 10, y1 = 2, x2 = 4, x1 = 2;
+// m = ((y2 - y1) / (x2 - x1))
+// console.log(m);
+// let bit;
+// gb15 = ((15 * 1024 * 1024 * 8000) + (" bit") );
+// console.log(gb15);
 
 
-secundend = (("seconded ") + ((15/3) * 8000) + " bit rate tataj chadahnee");
-console.log(secundend);
+// secundend = (("seconded ") + ((15/3) * 8000) + " bit rate tataj chadahnee");
+// console.log(secundend);
 
 
 //17//
 
+// let side1 = window.prompt('gurwaljnii side1?')
+// console.log(side1);
+// let side2 = window.prompt('gurwaljnii side2?')
+// console.log(side2);
+// let side3 = window.prompt('gurwaljnii side3?')
+// console.log(side3);
+
+// talbaiTriangle = (side1 * side2 * side3);
+// console.log(talbaiTriangle);
+
+
+
+// 18 celsius to fahrenheit//
+
+// let fahrenheit;
+
+// let celsius = window.prompt("Celsius ee bichne uu!")
+// fahrenheit = ((celsius * (9/5)) + 32);
+// console.log(fahrenheit);
+
+//18//
+
+// let celsius;
+
+// let fahrenheit = window.prompt("Fahrenheitee oruulna uu!")
+// celsius = (fahrenheit - 32) * (5/9)
+// console.log(celsius);
+
+
+// 19// asuudaltai nemehgui shuud ab gj garaad bn
+
+// let a = window.prompt("a too");
+// let b = window.prompt("b too");
+
+// sum = a + b;
+// console.log(sum);
+
+
+//20//
+
+// let cm = window.prompt("cm oruulna uu!")
+// inch = (cm/2.54);
+// console.log(inch);
+
+// let inch = window.prompt("inch oruulna uu!")
+// cm = (inch * 2.54);
+// console.log(cm);
+
+
+//21//
+let v;
+const pi;
+pi = 3.14;
+let r = window.prompt("radius ug")
+v = (4/3) * pi (r * r * r)
+console.log(v);
