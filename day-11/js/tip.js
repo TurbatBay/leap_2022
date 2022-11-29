970f3e55e0995f13273e4e83974f990fef2f4da2
TurbatBay <learnwithturu@gmail.com>
2022-11-29 23:02:33 +0800

    Ternary ruu halit orj uzlee hud bnaa margaash hiiy ymrtaich if deer bol bodlogoo bodtson

---
+++ day-11/js/tip.js
@@ -0,0 +1,43 @@
+let a = 3000 , 
+b = 27500 , 
+c = 100000, 
+nemegdel = 1,( ((5000 <= a && a <= 30000) nemegdel = (a * 15) / 100; ?) nemegdel : 
+(((0 <= a && a <= 4999) || (30001 <= a && a <= 1000000000000) ) nemegdel = (a * 25) / 100;) nemegdel );
+
+
+
+
+
+
+
+
+// //if deer testlej uzew
+// if (5000 <= a && a <= 30000) {
+//     nemegdel = (a * 15) / 100;
+//     console.log(nemegdel)
+// }  else if ((0 <= a && a <= 4999) || (30001 <= a && a <= 1000000000000) ) {
+//     nemegdel = (a * 25) / 100;
+//     console.log(nemegdel)
+// }
+
+
+// //b 
+
+// if (5000 <= b && b <= 30000) {
+//     nemegdel = (b * 15) / 100;
+//     console.log(nemegdel)
+// }  else if ((0 <= b && b <= 4999) || (30001 <= b && b <= 1000000000000) ) {
+//     nemegdel = (b * 25) / 100;
+//     console.log(nemegdel)
+// }
+
+
+// //c 
+
+// if (5000 <= c && c <= 30000) {
+//     nemegdel = (c * 15) / 100;
+//     console.log(nemegdel)
+// }  else if ((0 <= c && c <= 4999) || (30001 <= c && c <= 1000000000000) ) {
+//     nemegdel = (c * 25) / 100;
+//     console.log(nemegdel)
+// }
