a55ff802bf37f568b9008cab3d364a47dfd9928f
TurbatBay <learnwithturu@gmail.com>
2022-11-29 16:44:12 +0800

    Ugugdsun toonuudiin hamgiin ih bolon hamgiin bagiig hewle

---
+++ day-11/js/ih-baga.js
@@ -0,0 +1,24 @@
+// Ugugdsun toonuudiin hamgiin ih bolon hamgiin bagiig hewle
+
+let a = 15, b = 7, c = 10;
+
+
+if ((a < b) && c < b) {
+    if (a < c) {
+        console.log("hamgiin ih n " + b + "mun hamgiin baga n " + a)
+    } else {
+        console.log("hamgiin ih n " + b + " mun hamgiin baga n " + c)
+    }
+} else if ((b < a) && c < a) {
+    if (b < c) {
+        console.log("hamgiin ih n " + a + " mun hamgiin baga n " + b)
+    } else {
+        console.log("hamgiin ih n " + a + " mun hamgiin baga n " + c)
+    }
+} else if ((b < c) && a < c) {
+    if (b < a) {
+        console.log("hamgiin ih n " + c + " mun hamgiin baga n " + b)
+    } else {
+        console.log("hamgiin ih n " + c + " mun hamgiin baga n " + a)
+    }
+}
