82476a3d9671dea30263dab79802ed23d7adb097
TurbatBay <learnwithturu@gmail.com>
2022-11-29 16:57:12 +0800

    fixed equal thing

--- day-11/js/ih-baga.js
+++ day-11/js/ih-baga.js
@@ -1,22 +1,22 @@
 // Ugugdsun toonuudiin hamgiin ih bolon hamgiin bagiig hewle
 
-let a = 15, b = 7, c = 10;
+let a = 5, b = 5, c = 5;
 
 
-if ((a < b) && c < b) {
-    if (a < c) {
-        console.log("hamgiin ih n " + b + "mun hamgiin baga n " + a)
+if ((a <= b) && c <= b) {
+    if (a <= c) {
+        console.log("hamgiin ih n " + b + " mun hamgiin baga n " + a)
     } else {
         console.log("hamgiin ih n " + b + " mun hamgiin baga n " + c)
     }
-} else if ((b < a) && c < a) {
-    if (b < c) {
+} else if ((b <= a) && c <= a) {
+    if (b <= c) {
         console.log("hamgiin ih n " + a + " mun hamgiin baga n " + b)
     } else {
         console.log("hamgiin ih n " + a + " mun hamgiin baga n " + c)
     }
-} else if ((b < c) && a < c) {
-    if (b < a) {
+} else if ((b <= c) && a <= c) {
+    if (b <= a) {
         console.log("hamgiin ih n " + c + " mun hamgiin baga n " + b)
     } else {
         console.log("hamgiin ih n " + c + " mun hamgiin baga n " + a)
