4191b2a28c8eacd618b5448787e6587c6d8f2374
TurbatBay <learnwithturu@gmail.com>
2022-11-29 20:25:00 +0800

    temtseen assignment  duusgaw

---
+++ day-11/js/temtseen.js
@@ -0,0 +1,18 @@
+//Temtseen
+
+let a;
+let b;
+a = (1000)/3;
+b = (12000)/3;
+console.log("a bagiin dundaj onoo " + a)
+console.log("b bagiin dundaj onoo " + b)
+if(a>=100 && b>=100){
+     
+    if (a == b) 
+        console.log("2 bag tentslee")
+    else if(a<b)
+    console.log("b bag yllaa")
+ else 
+        console.log ("a bag yllaa")
+} else 
+    console.log("ylagch bhgu")
