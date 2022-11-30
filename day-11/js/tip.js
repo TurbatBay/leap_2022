8fdd96d3f3a5c3b6341e818411111fba74006770
TurbatBay <learnwithturu@gmail.com>
2022-11-30 16:15:04 +0800

    tested

--- day-11/js/tip.js
+++ day-11/js/tip.js
@@ -1,8 +1,8 @@
-let a = 3000 , 
-b = 27500 , 
-c = 100000, 
-nemegdel = 1,( ((5000 <= a && a <= 30000) nemegdel = (a * 15) / 100; ?) nemegdel : 
-(((0 <= a && a <= 4999) || (30001 <= a && a <= 1000000000000) ) nemegdel = (a * 25) / 100;) nemegdel );
+let a = 3000,
+    b = 27500,
+    c = 100000,
+    nemegdel = 1,(((5000 <= a && a <= 30000) nemegdel = (a * 15) / 100; ?) nemegdel:
+(((0 <= a && a <= 4999) || (30001 <= a && a <= 1000000000000)) nemegdel = (a * 25) / 100;) nemegdel );
 
 
 
@@ -21,7 +21,7 @@ nemegdel = 1,( ((5000 <= a && a <= 30000) nemegdel = (a * 15) / 100; ?) nemegdel
 // }
 
 
-// //b 
+// //b
 
 // if (5000 <= b && b <= 30000) {
 //     nemegdel = (b * 15) / 100;
@@ -32,7 +32,7 @@ nemegdel = 1,( ((5000 <= a && a <= 30000) nemegdel = (a * 15) / 100; ?) nemegdel
 // }
 
 
-// //c 
+// //c
 
 // if (5000 <= c && c <= 30000) {
 //     nemegdel = (c * 15) / 100;
