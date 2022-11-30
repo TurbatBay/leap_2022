59e87c91ce0e926f8cc0af7867f3a154fb8cdf86
TurbatBay <learnwithturu@gmail.com>
2022-12-01 00:51:46 +0800

    added codes to Ognoo hewleh assignment

--- day-12/js/main.js
+++ day-12/js/main.js
@@ -109,48 +109,50 @@
 
 
 
-let on, sar, udur;
+let on, sar, udur,sar1;
 
 on = prompt("On oruulna uu")
 sar = prompt("sar oruulna uu")
 udur = prompt("udur oruulna uu")
 
-if (sar < 12) {
+if (sar <= 12) {
     if (sar < 10) {
-        sar = "0" + sar
-    }
+        sar1 = "0" + sar
+    } else sar1=sar 
 } else console.log("sar chin hudlaa bn")
 if (sar == 2 && ((on % 4 == 0) && (on % 100 != 0) || (on % 400 == 0))) {
     if (udur <= 29) {
         if (udur < 10) {
             udur = "0" + udur
-            console.log(on + " " + sar + " " + udur)
-        } else console.log(on + " " + sar + " " + udur) 
-    } else console.log("tiim udur bhgu")
-        } else if (sar == 2 && ((on % 4 == 0) && (on % 100 != 0) && (on % 400 == 0))) {
-            if (udur <= 28) {
-                if (udur < 10) {
-                    udur = "0" + udur
-                    console.log(on + " " + sar + " " + udur)
-                } else console.log(on + " " + sar + " " + udur)
+            console.log(on + " " + sar1 + " " + udur)
+        } else console.log(on + " " + sar1 + " " + udur)
+    } else console.log("iim udur bhgu")
+} else {
+    if (udur <= 28) {
+        if (udur < 10) {
+            udur = "0" + udur
+            console.log(on + " " + sar1 + " " + udur)
+        } else console.log(on + " " + sar1 + " " + udur)
     }
-    else console.log("tiim udur bhgu")
-    
-}
-else if(sar==1 && sar==3 && sar==5 && sar==7 && sar==8 && sar==10 && sar==12)
-{ if(udur <= 31) {
-    if (udur < 10) {
-        udur = "0" + udur
-        console.log(on + " " + sar + " " + udur)
-    } else console.log(on + " " + sar + " " + udur)
+    else console.log("-")
 
-} else console.log("udur chin hudlaa bn") }
-else {if(udur <= 30) {
-    if (udur < 10) {
-        udur = "0" + udur
-        console.log(on + " " + sar + " " + udur)
-    }else console.log(on + " " + sar + " " + udur)
- } else console.log("tiim udur bhgu")
+}
+if (sar == 1 || sar == 3 || sar == 5 || sar == 7 || sar == 8 || sar == 10 || sar == 12) {
+    if (udur <= 31) {
+        if (udur < 10) {
+            udur = "0" + udur
+            console.log(on + " " + sar1 + " " + udur)
+        } else console.log(on + " " + sar1 + " " + udur)
+
+    } else console.log("udur chin hudlaa bn")
+}
+else {
+    if (udur <= 30) {
+        if (udur < 10) {
+            udur = "0" + udur
+            console.log(on + " " + sar1 + " " + udur)
+        } else console.log(on + " " + sar1 + " " + udur)
+    } else console.log("tiim udur bhgu")
 }
 
 
