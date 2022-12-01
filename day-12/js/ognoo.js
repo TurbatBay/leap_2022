5daa685a356c26e09f2cc95da4646150b34e95af
Morning <morning@mstars-lab2-14s-iMac.local>
2022-12-01 12:20:41 +0800

    day-13 angid

--- day-12/js/ognoo.js
+++ day-12/js/ognoo.js
@@ -16,30 +16,32 @@ if (sar == 2 && ((on % 4 == 0) && (on % 100 != 0) || (on % 400 == 0))) {
         if (udur < 10) {
             udur = "0" + udur
             console.log(on + " " + sar + " " + udur)
-        } else console.log(on + " " + sar + " " + udur) 
+        } else console.log(on + " " + sar + " " + udur)
     } else console.log("tiim udur bhgu")
-        } else if (sar == 2 && ((on % 4 == 0) && (on % 100 != 0) && (on % 400 == 0))) {
-            if (udur <= 28) {
-                if (udur < 10) {
-                    udur = "0" + udur
-                    console.log(on + " " + sar + " " + udur)
-                } else console.log(on + " " + sar + " " + udur)
+} else if (sar == 2 && ((on % 4 == 0) && (on % 100 != 0) && (on % 400 == 0))) {
+    if (udur <= 28) {
+        if (udur < 10) {
+            udur = "0" + udur
+            console.log(on + " " + sar + " " + udur)
+        } else console.log(on + " " + sar + " " + udur)
     }
     else console.log("tiim udur bhgu")
-    
-}
-else if(sar==1 && sar==3 && sar==5 && sar==7 && sar==8 && sar==10 && sar==12)
-{ if(udur <= 31) {
-    if (udur < 10) {
-        udur = "0" + udur
-        console.log(on + " " + sar + " " + udur)
-    } else console.log(on + " " + sar + " " + udur)
 
-} else console.log("udur chin hudlaa bn") }
-else {if(udur <= 30) {
-    if (udur < 10) {
-        udur = "0" + udur
-        console.log(on + " " + sar + " " + udur)
-    }else console.log(on + " " + sar + " " + udur)
- } else console.log("tiim udur bhgu")
+}
+else if (sar == 1 || sar == 3 || sar == 5 || sar == 7 || sar == 8 || sar == 10 || sar == 12) {
+    if (udur <= 31) {
+        if (udur < 10) {
+            udur = "0" + udur
+            console.log(on + " " + sar + " " + udur)
+        } else console.log(on + " " + sar + " " + udur)
+
+    } else console.log("udur chin hudlaa bn")
+}
+else {
+    if (udur <= 30) {
+        if (udur < 10) {
+            udur = "0" + udur
+            console.log(on + " " + sar + " " + udur)
+        } else console.log(on + " " + sar + " " + udur)
+    } else console.log("tiim udur bhgu")
 }
---
+++ day-13/js/main.js
@@ -0,0 +1,93 @@
+   // Day 13
+
+     //Math
+
+// console.log(Math.PI);
+// console.log(Math.PI.toFixed(2));
+
+// const budget = 10_000_000_000;   //zuwhun codert haragdana//
+
+// console.log(budget);
+
+// const degrees = 45;
+// let rad = degrees * (Math.PI / 180);
+// console.log(rad.toFixed(4))
+
+// let a = 5 , b = 9;
+// const diff = Math.abs(a - b);
+// console.log(diff);
+
+// console.log(Math.max(1 , 2, 7));
+
+
+// console.log(Math.min(1 , 2, 7))
+
+// let y  = Number(prompt("Give me y"));
+// let z  = Number(prompt("Give me z"));
+// let x = Math.sqrt((Math.pow(y ,2)) + (Math.pow(z , 2)));
+// console.log(x)
+
+
+// console.log(Math.ceil (Math.random()*100))
+
+
+
+           // If eer shalgasan n
+
+// let a = Number(prompt("a toog ugnu uu"));
+// let b = Number(prompt("b toog ugnu uu"));
+// let max, min;
+// if (max = a) {
+//     min=b
+// } else {
+//     max = b;
+//     min = a
+// }
+// console.log( Math.random() * (max-min) + min)
+
+          // math.max & math.min ashiglaw
+
+// let a = Number(prompt("a toog ugnu uu"));
+// let b = Number(prompt("b toog ugnu uu"));
+// let max , min;
+
+// max = Math.max (a,b);
+// min = Math.min (a,b);
+
+// console.log( Math.random() * (max-min) + min)
+
+
+//     // LOOOPS
+
+// let count = 1;
+// let howMany =
+// console.log("loop started")
+// while (count <= 50) {
+//     console.log("daraalal " + count);
+//     console.log(Math.round( Math.random() * (max-min) + min))
+//     count = count + 1;
+// }
+
+// console.log("loop ended")
+
+// let hariu =(prompt("ta togloomoo urgeljluuleh uu? tiim bol yes ugui bol no"))
+
+
+
+// while (hariu == "yes") {
+//     alert("thanks");
+//     hariu =(prompt("ta togloomoo urgeljluuleh uu? tiim bol yes ugui bol no"))
+// }
+// console.log("game-over");
+
+//inner zagdaa meme + lock
+
+let too = Number(prompt("Ehnii toogoo oruulna uu"));
+let sum = 0;
+
+while (too > 0) {
+    console.log(too);
+    sum = (too + sum);
+    too = too -1;
+}
+console.log("nemelt " + sum)
