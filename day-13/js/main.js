1802ef03dae98e46d2a5d327f233c11843535eb7
Morning <morning@mstars-lab2-14s-iMac.local>
2022-12-01 12:27:38 +0800

    adding toonii niilber bodlogo

--- day-13/js/main.js
+++ day-13/js/main.js
@@ -87,7 +87,7 @@ let sum = 0;
 
 while (too > 0) {
     console.log(too);
-    sum = (too + sum);
+    sum = (too + sum); //6 + 5 + 4 + 3 + 2 + 1 
     too = too -1;
 }
 console.log("nemelt " + sum)
