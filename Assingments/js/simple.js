6b4864769c2fc21e9b22875a06c7b19677f9c0ae
TurbatBay <learnwithturu@gmail.com>
2022-12-05 08:26:48 +0800

    assignment dotor factorial oloh bodlogo bodloo

--- Assingments/js/simple.js
+++ Assingments/js/simple.js
@@ -55,22 +55,70 @@
 
 //INTERWAL DAHI ANHNII TOOG HEWLEH
 
-let a = Number(prompt("a too"));
-let b = Number(prompt("b too"));
- // a=3 b=5
-let s;
-for (let sum = a; sum <= b; sum++) {
-  //3 4 5
-  let huwaagchid = 0;
-  for (let s = 1; s<sum; s++) {
-    if (sum % s == 0) {
-      //sum=3
-      huwaagchid++;
-    }
-  }
-  if (huwaagchid <= 2) {
-    console.log(sum);
-  }
-}
+// let a = Number(prompt("a too"));
+// let b = Number(prompt("b too"));
+//  // a=3 b=5
+// let s;
+// for (let sum = a; sum <= b; sum++) {
+//   //3 4 5
+//   let huwaagchid = 0;
+//   for (let s = 1; s<sum; s++) {
+//     if (sum % s == 0) {
+//       //sum=3
+//       huwaagchid++;
+//     }
+//   }
+//   if (huwaagchid <= 2) {
+//     console.log(sum);
+//   }
+// }
+
+
 /*a=10 b=17 h=0
-sum=13 s=1
+sum=13 s=1 */
+
+
+        // ugugdsun toonii tsifruudiig ugeer hewleh program bich//
+
+// let n = Number(prompt("n too garaas ug"));
+
+// for (sum = 0;sum <10;sum++) {
+//   if (sum == 0) {
+//     console.log("teg")
+//   } else if (sum == 1) {
+//     console.log("neg")
+//   } else if (sum == 2) {
+//     console.log("hoyr")
+//   } else if (sum == 3) {
+//     console.log("guraw")
+//   } else if (sum == 4) {
+//     console.log("duruw")
+//   } else if (sum == 5) {
+//     console.log("taw")
+//   } else if (sum == 6) {
+//     console.log("zurgaa")
+//   } else if (sum == 7) {
+//     console.log("doloo")
+//   } else if (sum == 8) {
+//     console.log("naim")
+//   } else if (sum == 9) {
+//     console.log("ys")
+//   }
+// }
+
+
+        //factorial oloh
+
+
+let n = Number(prompt("n toog garaas ugnu uu"));
+let i = 1;
+
+
+for (sum = 1; sum <= n; sum++) {  
+  i = i * sum;
+    
+    
+} 
+console.log(i);
+
+
