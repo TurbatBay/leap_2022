b8b6d1ed31f48dc96290fd95c1395c9c8332a810
TurbatBay <learnwithturu@gmail.com>
2022-12-06 00:24:34 +0800

    palindrom too oloh deer butarhaigui bolgoj zaslaa

--- day-13/day-13 homework/js/palindrom.js
+++ day-13/day-13 homework/js/palindrom.js
@@ -8,11 +8,11 @@ n = Number(prompt("Too ugnu uu"))
 let s = 0;
 let k = n;
 
-while (n > 0) {
+while (n > 0) { //n = 123
     let d = n % 10;
     s = s * 10 + d;
     //console.log(s);
-    let n = n / 10;
+    let n = Math.round (n / 10);
 
 }
 if (k == s) {
