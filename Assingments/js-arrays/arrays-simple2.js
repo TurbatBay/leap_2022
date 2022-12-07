19ca09c7be835ccf01e5be12c8e026c794db6571
TurbatBay <learnwithturu@gmail.com>
2022-12-08 01:15:05 +0800

    added arrays simple-2 but not done yet

---
+++ Assingments/js-arrays/arrays-simple2.js
@@ -0,0 +1,10 @@
+let itCompanies = ["Google", 'Facebook', "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+
+console.log(itCompanies[0]);
+console.log(itCompanies[1]);
+console.log(itCompanies[2]);
+
+console.log([itCompanies.length-1]);
+console.log([itCompanies.length-2]);
+console.log([itCompanies.length-3]);
